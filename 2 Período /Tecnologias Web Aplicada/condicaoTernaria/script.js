9ccb7031c1952e4f31485c68746d4875ffb23ad2
Luis Guilherme <guilherme32698@gmail.com>
2025-08-27 19:22:22 -0300

    desenvolvendo o uso diferente de if else

---
+++ 2 Período /Tecnologias Web Aplicada/condicaoTernaria/script.js
@@ -0,0 +1,13 @@
+nota = 1;
+nota2 = 7;
+
+if ( nota>= 7){
+    var Resultado = 'Aprovado';
+}
+else{
+    var Resultado = 'Reprovado';
+}
+
+var Resultado = (nota2 >= 7 )? Resultado = 'Aprovado' : Resultado = 'Recuperação';
+
+// variavel = (condicao) ? primeira_condicao : segunda_condicao;
