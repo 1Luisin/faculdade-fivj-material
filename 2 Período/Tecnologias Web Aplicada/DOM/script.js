dac00fbf06f168277df5db4f1954423567cfca8a
Luis Guilherme <guilherme32698@gmail.com>
2025-09-10 19:52:23 -0300

    aprendendo a interagir com elementos js x html

--- 2 Período/Tecnologias Web Aplicada/DOM/script.js
+++ 2 Período/Tecnologias Web Aplicada/DOM/script.js
@@ -4,3 +4,17 @@ console.log(subtitle);
 if (subtitle != null){
     subtitle.innerText = "Subtítulo Modificado :3";
 }
+
+let paragrafos = document.getElementsByTagName('p');
+
+paragrafos.forEach(paragrafo => {
+    
+});
+
+/*for (i = 0; i < paragrafos.length; i++){
+    console.log(paragrafos[i]);
+}*/
+// let textoVermelho = document.getElementsByClassName('text-red');
+
+
+
