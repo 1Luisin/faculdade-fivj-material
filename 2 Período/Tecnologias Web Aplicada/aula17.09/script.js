c72b390fdb84694ae5272aff8a005891752d6a93
Luis Guilherme <guilherme32698@gmail.com>
2025-09-17 19:25:07 -0300

    aula 17.09

---
+++ 2 Período/Tecnologias Web Aplicada/aula17.09/script.js
@@ -0,0 +1,19 @@
+function modoEscuro(){
+    const corpo = document.querySelector('body');
+    corpo.classList.toggle('dark-mode');
+}
+
+const titulo1 = document.querySelector('h1');
+const titulo2 = document.querySelector('h2')
+
+titulo1.addEventListener("click", ()=>{
+    console.log('clicou.');
+})
+
+// ou
+
+function clicouRetornou(){
+    console.log('clicou no h2');
+}
+
+titulo2.addEventListener("click",clicouRetornou());
