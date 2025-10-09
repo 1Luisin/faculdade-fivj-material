da85bd9da0bba42dcff0fa9f84c58bd192219af7
Luis Guilherme <guilherme32698@gmail.com>
2025-10-09 08:10:41 -0300

    atualizando branch

---
+++ 2 Período/Programação OO/pratica06bluej/praticaBlueJ/Cachorro.java
@@ -0,0 +1,10 @@
+public class Cachorro implements Animal{   
+    @Override
+    public void dormir(){
+    }
+    
+    @Override
+    public void fazerSom(){
+    }
+
+}
---
+++ 2 Período/Programação OO/pratica06bluej/praticaBlueJ/Patas.java
@@ -0,0 +1,12 @@
+public class Patas{
+    
+    private Integer patas;
+    
+    public Integer getPatas(){
+        return patas;
+    }
+    
+    public void setPatas(Integer patas){
+        this.patas = patas;
+    }
+}
