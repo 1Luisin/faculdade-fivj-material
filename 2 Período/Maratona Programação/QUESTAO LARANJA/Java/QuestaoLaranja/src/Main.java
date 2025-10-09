9d10e07a7b6f52d22080dd1a0837dc569472fa74
Luis Guilherme <guilherme32698@gmail.com>
2025-10-09 17:24:08 -0300

    Lógica da questão Laranja aplicada, e finalizada

--- 2 Período/Maratona Programação/QUESTAO LARANJA/Java/QuestaoLaranja/src/Main.java
+++ 2 Período/Maratona Programação/QUESTAO LARANJA/Java/QuestaoLaranja/src/Main.java
@@ -1,7 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int numero1 = input.nextInt();
+        int numero2 = input.nextInt();
+
+       if (numero2 == numero1 + 1 || numero2 == numero1 - 1) {
+           System.out.println(1);
+       }
+        else {
+            System.out.println(0);
+        }
     }
 }
