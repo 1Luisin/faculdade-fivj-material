460b0ce21dcb04b7880f31f0aec0c965a8ca651e
Luis Guilherme <guilherme32698@gmail.com>
2025-10-10 10:39:49 -0300

    estrutura do exercicio verde quase completa

--- 2 Período/Maratona Programação/ QUESTAO VERDE/Java/QuestaoVerde/src/Main.java
+++ 2 Período/Maratona Programação/ QUESTAO VERDE/Java/QuestaoVerde/src/Main.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int numeroUsuario = input.nextInt();
 
-        while(numeroUsuario != 0){
-            int maiornumero = 0;
-            numeroUsuario = input.nextInt();
-                if(numero)
-            {}
+        Scanner input = new Scanner(System.in);
+        int numeroUsuario = input.nextInt(); //recebendo numero inicial usuario
+        int maiorNum = 0;
+
+        while(numeroUsuario != 0) {
+
+        numeroUsuario = input.nextInt(); // recebendo numero de usuario
+        maiorNum = numeroUsuario; 
+
         }
+        System.out.println(maiorNum);
     }
 }
