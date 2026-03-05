ec6afe386c85411fc098941ebbfa75a2afe95037
Luis Guilherme <guilherme32698@gmail.com>
2026-03-05 19:51:02 -0300

    entendendo a estrutura for

---
+++ 3 Período/Tecnologias atuais em desenvolvimento de software/aula5marco/foreachdopythonkkk.py
@@ -0,0 +1,10 @@
+amigos = ['Huguinho','Zezinho','Luizinho','Margarida']
+
+for amigo in amigos:
+    if amigo == 'Margarida':
+        print("Feliz dia das mulheres.")
+    else:
+        print(f"Sou amigo do {amigo}")
+        print(f"Olá {amigo}, é muito bom ser seu amigo")
+    
+    
