03592a941e4e72ada9f8fee38922b29a8c4e83ab
Luis Guilherme <guilherme32698@gmail.com>
2026-03-11 19:28:03 -0300

    atualizando commits faculdade

--- 3 Período/Tecnologias atuais em desenvolvimento de software/aula5marco/foreachdopythonkkk.py
+++ 3 Período/Tecnologias atuais em desenvolvimento de software/aula5marco/foreachdopythonkkk.py
@@ -6,5 +6,5 @@ for amigo in amigos:
     else:
         print(f"Sou amigo do {amigo}")
         print(f"Olá {amigo}, é muito bom ser seu amigo")
-    
-    
+        
+print(f"Tenho {len(amigos)} INIMIGOS >:) HAHAHA")
---
+++ 3 Período/Tecnologias atuais em desenvolvimento de software/aula5marco/listasnumericas.py
@@ -0,0 +1,18 @@
+for value in range (1,5):
+    print(value)
+
+for value2 in range (5):
+    print (value2)
+
+for value3 in range (10,0):
+    print (value3)
+
+
+numeros = list(range(0,10))
+
+listavazia = []
+
+for i in range(1,11):
+    listavazia.append(i ** 2)
+
+print (listavazia)
